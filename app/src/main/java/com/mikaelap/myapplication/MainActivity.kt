65aefa5030b056a015cab96f1c4175c622244308
Diana <livengoodd@merrimack.edu>
2026-03-17 16:42:06 -0400

    Added images and reworked UI. Will add buttons after a short break.

--- app/src/main/java/com/mikaelap/myapplication/MainActivity.kt
+++ app/src/main/java/com/mikaelap/myapplication/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.mikaelap.myapplication
 
+import android.R.id
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,41 +20,31 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.runtime.*
 import androidx.lifecycle.ViewModelProvider
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontStyle
-import com.mikaelap.myapplication.MyApplicationTheme
-import kotlin.collections.get
-import androidx.compose.material3.AlertDialog
 
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 
 val TimesNewRoman = FontFamily(
     Font(R.font.times, FontWeight.Normal),
@@ -110,7 +102,9 @@ fun MainScreen(viewModel: MainViewModel) {
             }
         }
     ) { innerPadding ->
-        Box(modifier = Modifier.padding(innerPadding)) {
+        Box(modifier = Modifier
+            .padding(innerPadding)
+            .border(width = 2.dp, color = Color(0xFF1A2C57))) {
             when (selectedTabIndex) {
                 0 -> TriviaScreen(viewModel = viewModel)
                 1 -> AcadScreen()
@@ -151,104 +145,103 @@ fun FameScreen(modifier: Modifier = Modifier) {
     ) {
         TopBanner()
         FameBanner()
-
-        Row {
-            Text(
-                text = "National Ranking & Academics",
-                fontFamily = TimesNewRoman,
-                fontSize = 18.sp
-            )
-
-            Spacer(modifier = Modifier.width(10.dp))
-
-            Button(onClick = { showDialog = true }) {
-                Text("Learn More...")
-            }
-        }
-
-        Spacer(modifier = Modifier.height(10.dp))
-
-        Row {
-            Text(
-                text = "Sustainability",
-                fontFamily = TimesNewRoman,
-                fontSize = 18.sp
-            )
-
-            Spacer(modifier = Modifier.width(10.dp))
-
-            Button(onClick = { showDialog = true }) {
-                Text("Learn More...")
-            }
-        }
-
-        Spacer(modifier = Modifier.height(10.dp))
-
-        Row {
-            Text(
-                text = "Athletics",
-                fontFamily = TimesNewRoman,
-                fontSize = 18.sp
-            )
-
-            Spacer(modifier = Modifier.width(10.dp))
-
-            Button(onClick = { showDialog = true }) {
-                Text("Learn More...")
-            }
-        }
-
-        Spacer(modifier = Modifier.height(10.dp))
-
-        Row {
-            Text(
-                text = "Sylvia Plath",
-                fontFamily = TimesNewRoman,
-                fontSize = 18.sp
-            )
-
-            Spacer(modifier = Modifier.width(10.dp))
-
-            Button(onClick = { showDialog = true }) {
-                Text("Learn More...")
-            }
-        }
-
-        Spacer(modifier = Modifier.height(10.dp))
-
-        Row {
-            Text(
-                text = "Nancy Reagan",
-                fontFamily = TimesNewRoman,
-                fontSize = 18.sp
-            )
-
-            Spacer(modifier = Modifier.width(10.dp))
-
-            Button(onClick = { showDialog = true }) {
-                Text("Learn More...")
-            }
-        }
-
-        Spacer(modifier = Modifier.height(10.dp))
-
-        Row {
-            Text(
-                text = "Julia Child",
-                fontFamily = TimesNewRoman,
-                fontSize = 18.sp
-            )
-
-            Spacer(modifier = Modifier.width(10.dp))
-
-            Button(onClick = { showDialog = true }) {
-                Text("Learn More...")
-            }
-        }
-
-        Spacer(modifier = Modifier.height(10.dp))
+        Factoids()
     }
 }
+
+@Composable
+fun FactoidBoxes(
+    modifier: Modifier = Modifier,
+    text: String,
+    backgroundImage: Int? = null
+) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = Color(0xFF97CDEC))
+            .border(width = 3.dp, color = Color(0xFF1A2C57)),
+        contentAlignment = Alignment.Center
+    ) {
+        if (backgroundImage != null) {
+            Image(
+                painter = painterResource(id = backgroundImage),
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black.copy(alpha = 0.85f))
+        )
+        Text(
+            text = text,
+            color = Color(0xAFFFFFFF),
+            fontFamily = TimesNewRoman,
+            fontSize = 18.sp,
+            textAlign = TextAlign.Center
+        )
+
+    }
+}
+
+@Composable
+fun Factoids() {
+    Column(
+        modifier = Modifier
+            //.fillMaxSize() makes it appear like option 1 in group text
+            //.background(Color(0xFF1A2C57))
+    ) {
+        Row(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            FactoidBoxes(
+                modifier = Modifier.weight(1f),
+                text = "National Ranking & Academics",
+                backgroundImage = R.drawable.academics
+
+            )
+            FactoidBoxes(
+                modifier = Modifier.weight(1f),
+                text = "Sustainability",
+                backgroundImage = R.drawable.smithpark
+            )
+            FactoidBoxes(
+                modifier = Modifier.weight(1f),
+                text = "Athletics",
+                backgroundImage = R.drawable.athletics
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            FactoidBoxes(
+                modifier = Modifier.weight(1f),
+                text = "Sylvia Plath",
+                backgroundImage = R.drawable.sylviaplath
+            )
+            FactoidBoxes(
+                modifier = Modifier.weight(1f),
+                text = "Julia Child",
+                backgroundImage = R.drawable.juliachild
+            )
+            FactoidBoxes(
+                modifier = Modifier.weight(1f),
+                text = "Nancy Reagan",
+                backgroundImage = R.drawable.nancyreagan
+            )
+        }
+    }
+}
+
 
 @Composable
 fun TriviaScreen(
